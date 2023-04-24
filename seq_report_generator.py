e9540156535c9d7c7e5e7b72d8838f7b80b5a2ef
Holly Halstead <88352855+DOH-HNH0303@users.noreply.github.com>
2023-04-23 19:55:33 -0700

    Update seq_report_generator.py

--- seq_report_generator.py
+++ seq_report_generator.py
@@ -177,10 +177,13 @@ def join_pdfs(list, output_filename):
 
 def remove_nan_from_list(l):
     return [item for item in l if str(item) != 'nan']
-
-def unique(list1):
-    x = np.array(list1)
-    return list(np.unique(x))
+  
+def unique(list1): 
+    unique_list = []
+    for x in list1:
+        if x not in unique_list:
+            unique_list.append(x)
+    return unique_list
 
 def _add_spaces(list, maxlen):
     head_list = []
